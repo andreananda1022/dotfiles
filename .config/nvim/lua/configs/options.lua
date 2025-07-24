2334fca5900f0f889c8c7ad1bc6a93b4d4f53c25
andreananda1022 <andreananda1022@gmail.com>
2025-07-24 17:19:34 +0700

    Add files via upload

---
+++ .config/nvim/lua/configs/options.lua
@@ -0,0 +1,19 @@
+vim.opt.wrap = false
+vim.opt.number = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.fillchars = { eob = " " }
+
+vim.cmd("colorscheme tokyonight")
+vim.opt.termguicolors = true
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+
+vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NeoTreeFloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "none" })
