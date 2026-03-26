cab9ec2da376c93b50989fb7458cc8dad9bfeac4
andreananda1022 <andreananda1022@gmail.com>
2026-03-26 10:58:29 +0700

    Add files via upload

---
+++ bubbleSort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n);
+
+int main() {
+    int arr[6] = {43, 25, 74, 35, 36, 24};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    bubbleSort(arr, n);
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
